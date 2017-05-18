67fe71933eeec7c2a8d76be7e29d567e2b9b27b2
Elygian <lucasarchboldjones@gmail.com>
2017-05-18 16:41:50 +0100

    Add current version of script
    Added the code written so far

---
+++ ProcessKiller.ps1
@@ -0,0 +1,34 @@
+﻿param(
+    [string]$serviceToKill
+    )
+
+$ProcessId = get-wmiobject win32_service | where {$_.name -eq $serviceToKill} | Select-Object -expand ProcessId
+
+
+Try {
+
+    if ($ProcessId -ne 0) {
+    Write-Host "Killing process: $serviceToKill"
+        Stop-Process -id $ProcessId -Force | Wait-Process $ProcessId
+
+        $isStopped = get-wmiobject win32_service | where {$_.name -eq $serviceToKill} | Select-Object -expand ProcessId
+
+        if ($isStopped -eq 0) {
+            Start-Service -name $serviceToKill
+            Write-Host "Service starting..."
+        }
+
+    }
+
+    Else {
+           Write-Host "Process $serviceToKill is not running. Starting process"
+           Start-Service -name $serviceToKill
+         }
+
+}
+
+Catch {
+
+    Write-Host "Error: Parameter passed is null, please check arguments"
+
+}
